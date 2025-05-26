34e1e6dfd2b1ce40550724ff39d4b625435f913e
QASIM <qasimgardezi04@gmail.com>
2025-05-27 00:39:58 +0500

    working on routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
-
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { StudentsComponent } from './components/students/students.component'; // 👈 import student component
+import { StudentsComponent } from './components/students/students.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full' // very important
+  },
   {
     path: 'auth',
     loadChildren: () =>
       import('./auth/auth.module').then((m) => m.AuthModule),
   },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'student-dashboard', component: StudentsComponent } // 👈 student dashboard ka route
+  { path: 'student-dashboard', component: StudentsComponent }
 ];
